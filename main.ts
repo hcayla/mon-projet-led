5cd0f9ab7451b327cc27302f1926db38e0a74161
hcayla <128042961+hcayla@users.noreply.github.com>
2023-03-16 16:40:03 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
-input.onButtonPressed(Button.A, function () {
-    gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(500)
-    gauche.showColor(neopixel.colors(NeoPixelColors.Black))
-})
-input.onButtonPressed(Button.AB, function () {
-    stop.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(500)
-    stop.showColor(neopixel.colors(NeoPixelColors.Black))
-})
-input.onButtonPressed(Button.B, function () {
+function clignote_droite () {
     droite.showColor(neopixel.colors(NeoPixelColors.Orange))
     basic.pause(500)
     droite.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(500)
+}
+function clignote_gauche () {
+    gauche.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(500)
+    gauche.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.pause(500)
+}
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 3; index++) {
+        clignote_gauche()
+    }
+})
+function clignote_stop () {
+    stop.showColor(neopixel.colors(NeoPixelColors.Red))
+}
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 3; index++) {
+        clignote_stop()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 3; index++) {
+        clignote_droite()
+    }
 })
 let droite: neopixel.Strip = null
 let stop: neopixel.Strip = null
